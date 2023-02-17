0d4b8477e3173bec98982edc08efd351d93b2a3f
Bedasa <bedonaf@gmail.com>
2023-02-17 03:44:29 -0800

    Last commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 /**
- * main entry point
- * return 0
+ * main - Entry point
+ *
+ * Description: prints text
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
