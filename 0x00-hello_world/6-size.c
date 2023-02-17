83c907db0c3bdd5d11c9f2bd1ddc4193b3c5e1d6
Bedasa <bedonaf@gmail.com>
2023-02-17 04:03:52 -0800

    Size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+
 /**
- *main - Entry point
+ * main - Entry point
+ * 
+ * Decription: size of datatype
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int a;
