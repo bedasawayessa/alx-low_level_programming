7bfe30c5f9521fad2682fcd92f792be5fe944bbf
Bedasa Wayessa <bedonaf@gmail.com>
2023-06-20 03:29:26 -0700

    1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -c -fPIC *.c
+#!/bin/bash
+gcc -c -Wall -Werror -Wextra -pedantic -fpic *.c
 gcc -shared -o liball.so *.o
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
